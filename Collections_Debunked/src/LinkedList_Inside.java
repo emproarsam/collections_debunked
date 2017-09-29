4194498d22cb5f972e2b14b557635650d615a6fd
samarth <samarth@192.168.0.103>
2017-09-29 13:22:50 +0530

    Collections Debunked to repo

---
+++ Collections_Debunked/src/LinkedList_Inside.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LinkedList_Inside {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new ArrayList();
+	}
+
+}
